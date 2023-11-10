bf83d85317063e910bc71c5d0badeaa6130976fc
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-11-10 17:49:25 +0100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const prompt = require("prompt-sync")();
 
-const target = Math.random();
+const target = Math.round(Math.random() * 100);
+
+let guesses = 0; 
 console.log(target);
+
+const guess = Number(prompt("Guess a number(0-100): "));
+
+if(guess > target){
+     console.log("Too High.");
+}else{
+    console.log("Too low.");
+}
+console.log(guess);
 
