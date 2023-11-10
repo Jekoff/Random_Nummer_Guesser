626f12a3401ab8ccd841d8359dfeb9ccaedc4139
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-11-10 23:50:41 +0100

    commit

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ const prompt = require("prompt-sync")();
 const target = Math.round(Math.random() * 100);
 
 let guesses = 0; 
-console.log(target);
 
-const guess = Number(prompt("Guess a number(0-100): "));
+while (true) {
+    guesses++;
 
-if(guess > target){
-     console.log("Too High.");
-}else if(guess < target){
-    console.log("Too low.");
-}else{
-    console.log("You guessed it!");
+    const input = prompt("Guess a number(0-100): ");
+    const guess = Number(input);
+
+    // Check if the input is not a number
+    if (isNaN(guess)) {
+        console.log("That's not a valid number. Try again.");
+        continue; // Skip the rest of the loop and prompt again
+    }
+
+    if (guess > target) {
+        console.log("Too High.");
+    } else if (guess < target) {
+        console.log("Too low.");
+    } else {
+        console.log("You guessed it!");
+        break;
+    }
 }
-   
 
+console.log("You guessed the number in", guesses, "tries!");
